48d4cb6980c926c0d4ae539df6e39f7ad432ab7b
Youssef <youssefbarsoom@gmail.com>
2022-01-27 00:54:58 +0200

    update6

--- server.js
+++ server.js
@@ -36,12 +36,19 @@ app.get('/SignUp',(req,res)=>{
     res.render('signUpPage');
 }) 
 app.get('/',(req,res)=>{
-  const JSONobject = ' {  "eventTitle":"Breaking Bad", "eventImage":"https://occ-0-1433-1432.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABUjsKDiaKwLmrV662pwmVKEtRmbJI-s8M9ojCqr2QEdnPUJPX86RP-n9IGXxGcaHWkTf-cwz5e4kBLN3jYLM7HuBfYA.webp?r=01d"   } '
+  const JSONobject = { "Popular":[{ "eventTitle":"Breaking Bad", "eventImage":"https://occ-0-1433-1432.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABUjsKDiaKwLmrV662pwmVKEtRmbJI-s8M9ojCqr2QEdnPUJPX86RP-n9IGXxGcaHWkTf-cwz5e4kBLN3jYLM7HuBfYA.webp?r=01d"  }],
+                      "New":[{ "eventTitle":"Breaking Bad New", "eventImage":"https://occ-0-1433-1432.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABUjsKDiaKwLmrV662pwmVKEtRmbJI-s8M9ojCqr2QEdnPUJPX86RP-n9IGXxGcaHWkTf-cwz5e4kBLN3jYLM7HuBfYA.webp?r=01d"  }]
+        } 
   
-  const s = JSON.parse(JSONobject)
+ // const s = JSON.parse(JSONobject)
  // return res.status(422).jsonp(s.eventTitle);
 // const d = s.array()
-    res.render('mainPlatform',{ lists:s});
+var sql = "SELECT * FROM CREATOR_EVENTS GROUP BY event;";
+connection.query(sql, function (err, result) {
+  if (err) throw err;
+  console.log("1 record inserted");
+});
+    res.render('mainPlatform',{ lists:JSONobject});
 }) 
 
 app.post('/signUpProcess',[
@@ -73,7 +80,31 @@ app.post('/signUpProcess',[
   }
 });
 
+function getVideos (){
+  var sql = "SELECT e.eventId,c.categoryName,c.categoryID,e.eventName,e.eventLink,e.eventImageLink,e.eventdate,eventDescrip,ticketPrice,ratingCount FROM CREATOR_EVENT e INNER JOIN VIDEO_CAT c on e.categoryID=c.categoryID GROUP BY e.categoryID;";
+  var returnObject ={}
+  var arrayOFEvents =[]
+  connection.query(sql, function (err, result) {
+    var curCat = result[0].categoryID
+    result.forEach(element => {
+      if(curCat == element.categoryID)
+      {
+arrayOFEvents.push({ "eventTitle": element.eventName, "eventImage":element.eventImageLink  })
+      }
+      else
+      {
+        returnObject[curCat] = arrayOFEvents;
+        arrayOFEvents=[];
+        arrayOFEvents.push({ "eventTitle": element.eventName, "eventImage":element.eventImageLink  })
+        curCat=element.categoryID;
+      }
+    });
+    
 
+  });
+
+  return returnObject;
+}
 
 const port = process.env.PORT||8081
 app.listen(port,()=>{
