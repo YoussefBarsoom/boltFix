f7651da77cc7481a7237f798337c1480b946dc94
Youssef <youssefbarsoom@gmail.com>
2022-01-26 03:12:00 +0200

    update6

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express= require('express')
 const app = express()
 const path=require('path')
-
-// var mysql=require('mysql');
-//  var connection=mysql.createConnection({
-//    host:process.env.HOST_NAME,
-//    user:process.env.DB_USERNAME,
-//    password:process.env.USER_PASSWORD,
-//    port:process.env.DB_PORT,
-//    database:process.env.DB_NAME
-//  });
-// connection.connect(function(error){
-//    if(!!error){
-//      console.log(error);
-//    }else{
-//      console.log('Connected!:)');
-//    }
-//  });  
-//module.exports = connection; 
+const bodyParser = require('body-parser')
+const {check,validationResult}=require('express-validator')
+var mysql=require('mysql');
+ var connection=mysql.createConnection({
+   host:process.env.HOST_NAME,
+   user:process.env.DB_USERNAME,
+   password:process.env.USER_PASSWORD,
+   port:process.env.DB_PORT,
+   database:process.env.DB_NAME
+ });
+connection.connect(function(error){
+   if(!!error){
+     console.log(error);
+   }else{
+     console.log('Connected!:)');
+   }
+ });  
+module.exports = connection; 
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 app.use(express.static(__dirname));
@@ -31,13 +32,37 @@ app.use(express.json())
 
 
 
-app.set("view engine", "ejs");
+app.get('/SignUp',(req,res)=>{
+    res.render('signUpPage');
+}) 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/views/signUpPage.html');
+  const JSONobject = ' {  "eventTitle":"Breaking Bad", "eventImage":"https://occ-0-1433-1432.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABUjsKDiaKwLmrV662pwmVKEtRmbJI-s8M9ojCqr2QEdnPUJPX86RP-n9IGXxGcaHWkTf-cwz5e4kBLN3jYLM7HuBfYA.webp?r=01d"   } '
+  
+  const s = JSON.parse(JSONobject)
+ // return res.status(422).jsonp(s.eventTitle);
+// const d = s.array()
+    res.render('mainPlatform',{ lists:s});
 }) 
 
-app.post('/signUpProcess', function(req, res){ // Specifies which URL to listen for
+app.post('/signUpProcess',[
+  check('passwordInput','Pasword must be 8+ characters long').exists().isLength({min:8}),
+  check('emailInput','Email is invalid').isEmail().normalizeEmail(),
+  check('firstNmeInput','First Name cannot be empty').exists().isLength({min:1}),
+  check('lastNmeInput','Last Name cannot be empty').exists().isLength({min:1})
+], function(req, res){ // Specifies which URL to listen for
     // req.body -- contains form data
+
+    const error= validationResult(req)
+    if(!error.isEmpty())
+    {
+      const alert = error.array()
+      res.render('signUpPage',{
+        alert
+      })
+    }
+    else{
+
+    
     var object = req.body;
     var sql = "insert into USERS(firstname,lastname,email,userPassword,userTypeID) Values ('"+object.firstNmeInput+"','"+object.lastNmeInput+"','"+object.emailInput+"','"+object.passwordInput+"',1)";
     connection.query(sql, function (err, result) {
@@ -45,6 +70,7 @@ app.post('/signUpProcess', function(req, res){ // Specifies which URL to listen 
       console.log("1 record inserted");
     });
     console.log(JSON.stringify(req.body)+" ");
+  }
 });
 
 
